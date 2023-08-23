d5c77496fbf31660deec110de778b3b4e9b7663c
NatalieMusilova <75503881+NatalieMusilova@users.noreply.github.com>
2023-08-23 14:50:57 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -35,3 +35,17 @@ def get_conversation_string():
         conversation_string += "Human: "+st.session_state['requests'][i] + "\n"
         conversation_string += "Bot: "+ st.session_state['responses'][i+1] + "\n"
     return conversation_string
+
+who_are_you = [
+"kdo jsi", "jsi bot", "kdo jsi?", "jsi bot?", "kdo jis?", "kdo jseš?", "ty si nějaký bot", "ty si nějaký bot?", "ty jsi robot?", "jsi člověk?", "jsi clovek?", 
+"jsi člověk", "jsi clovek", "jsi AI?", "AI?", "bot?", "co umíš", "co umíš?", "co umis", "co umis?",  "coumíš", "coumis", "coumíš?", "coumis?", "co umíš dělat?", "co všechno umíš?", 
+"jaké máš dovedností?", "co umiš", "co umiš?", "co umís", "co umís?", "kdo jseš", "kdo jses?", "kdo jses", "jsi robot?", "jsi robot", "co vše umíš?", "co všechno umíš", "co vše umíš"
+]
+who_are_you_res = """Jsem chatbot, vytvořený Fakultou informatiky a statistiky. Moje hlavní úloha spočívá v tom, abych reagoval na otázky a na základě dostupných zdrojů poskytoval informace. 
+
+V současné době pracuji s omezeným množstvím informací, avšak ty se neustále rozšiřují a zlepšují, abych mohl nabídnout co nejlepší pomoc.
+
+Pokud máte zájem o informace týkající se programů na Fakultě informatiky a statistiky, jsem schopen vám poskytnout některé základní informace o dostupných programech a jejich charakteristikách.
+
+Co se týče bodových limitů pro přijetí na fakultu, ty se každý rok mohou lišit v závislosti na mnoha faktorech. Poslední informace, které mám ohledně bodových hranic pro přijetí, se týkají přijímacího řízení na akademický rok 2023/2024. 
+"""
